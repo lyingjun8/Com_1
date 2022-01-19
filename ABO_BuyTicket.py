3bc7a6c173522b14aaf027bb596314aa15e4341e
lyingjun8 <98015610+lyingjun8@users.noreply.github.com>
2022-01-19 17:00:30 +0800

    Delete ABO_BuyTicket.py

--- ABO_BuyTicket.py
+++
@@ -1,8 +0,0 @@
-from Comment.Public import *
-
-
-env = input("环境:")
-market = input("市场:")
-
-SearchActivity_URL = 'https://bookingapi-' + env + '.amway.com/' + market + '/eventbooking/events/searchAvalaibleEvents'
-
