b882e628d5da7c1969a5afa8ce070b7e155c4997
lyingjun8 <98015610+lyingjun8@users.noreply.github.com>
2022-01-19 16:59:55 +0800

    Add files via upload

---
+++ ABO_BuyTicket.py
@@ -0,0 +1,8 @@
+from Comment.Public import *
+
+
+env = input("环境:")
+market = input("市场:")
+
+SearchActivity_URL = 'https://bookingapi-' + env + '.amway.com/' + market + '/eventbooking/events/searchAvalaibleEvents'
+
